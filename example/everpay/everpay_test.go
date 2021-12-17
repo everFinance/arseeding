15267cdf18cb70b52d1a33996592420593b54838
sandy <18382255942@163.com>
2021-12-17 19:29:59 +0800

    feat(): debug

--- example/everpay/everpay_test.go
+++ example/everpay/everpay_test.go
@@ -1,14 +1,10 @@
 package everpay
 
-import (
-	"testing"
-)
-
-func Test_EverPay(t *testing.T) {
-	dsn := "root@tcp(127.0.0.1:3306)/sandy_test?charset=utf8mb4&parseTime=True&loc=Local"
-	seedUrl := "https://seed-dev.everpay.io"
-	everpay := New(dsn, seedUrl)
-	everpay.Run()
-
-	select {}
-}
+// func Test_EverPay(t *testing.T) {
+// 	dsn := "root@tcp(127.0.0.1:3306)/sandy_test?charset=utf8mb4&parseTime=True&loc=Local"
+// 	seedUrl := "https://seed-dev.everpay.io"
+// 	everpay := New(dsn, seedUrl)
+// 	everpay.Run()
+//
+// 	select {}
+// }
